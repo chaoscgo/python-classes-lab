75b51b09cb627be6528c1bf1fe08b105baf13708
Beverly <chaoscgo@hotmail.com>
2025-01-28 11:00:25 -0600

    added check winner function

--- exercises.py
+++ exercises.py
@@ -8,9 +8,9 @@ class Game():
 
 
     def __init__(self, turn='X', tie = False, winner = 'None'):
-        self.turn = turn
-        self.tie = tie
-        self.winner = winner
+      self.turn = turn
+      self.tie = tie
+      self.winner = winner
 
     def print_board(self):
       b = self.board
@@ -31,13 +31,63 @@ class Game():
       else:
         print(f"It's player {self.turn}'s turn!")
 
-    def play_game(self, instance):
-        print('Welcome to Tic Tac Toe')
-        instance.print_board()
-        instance.print_message()
+
+    def get_move(self):
+        
+      while True:
 
         move = input(f"Enter a valid move (example: A1): ").lower()
-          if move is in [a1, a2, a3, b1, b2, b3, c1, c2, c3]:
+        
+        if move in {'a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'} and self.board[move] == None:
+        #   print(f"Previous board value: {self.board[move]}")
+          self.board[move] = self.turn
+        #   print(f"New board value: {self.board[move]}")
+          break
+
+        else:
+           print('That is an invalid input, try again!')
+
+    def check_winner(self):
+      if self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['a2'] and (self.board['a2'] == self.board['b2'] == self.board['c2']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['a3'] and (self.board['a3'] == self.board['b3'] == self.board['c3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['a1'] and (self.board['a1'] == self.board['a2'] == self.board['a3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['b1'] and (self.board['b1'] == self.board['b2'] == self.board['b3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['c1'] and (self.board['c1'] == self.board['c2'] == self.board['c3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['a1'] and (self.board['a1'] == self.board['b2'] == self.board['c3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      elif self.board['c1'] and (self.board['c1'] == self.board['b2'] == self.board['a3']):
+        self.winner = self.turn
+        print(f"{self.winner} is the winner!")
+      else:
+        print('No winner yet!')
+
+    def check_tie(self):
+      
+
+
+    # def play_game(self, instance):
+    #   print('Welcome to Tic Tac Toe')
+    #   instance.print_board()
+    #   instance.print_message()
+    #   instance.get_move()
+
+           
+           
+           
         
 
     
@@ -46,6 +96,31 @@ game_instance = Game()
 
 game_instance1 = Game()
 
-game_instance.play_game(game_instance1)
+# game_instance.play_game(game_instance1)
+
+# game_instance.print_board()
+
+game_instance.print_message()
+
+game_instance.get_move()
+
+game_instance.print_board()
+
+game_instance.check_winner()
+
+game_instance.get_move()
+
+game_instance.print_board()
+
+game_instance.check_winner()
+
+game_instance.get_move()
+
+game_instance.print_board()
+
+game_instance.check_winner()
+
+
+
 
 
